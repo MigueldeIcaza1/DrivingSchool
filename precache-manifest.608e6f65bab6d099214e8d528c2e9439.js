cf1c7c93a673bac6280e47ec8d68c5ed4d96033b
mahalakshmi.gunti <mahalakshmi.gunti@ggktech.com>
2020-02-20 21:52:32 +0530

    Updates

---
+++ precache-manifest.608e6f65bab6d099214e8d528c2e9439.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93f00fb61592cca17f859b38850343a3",
+    "url": "/DrivingSchool/index.html"
+  },
+  {
+    "revision": "607ae823f9515955468e",
+    "url": "/DrivingSchool/static/css/main.400a92d6.chunk.css"
+  },
+  {
+    "revision": "f678d9c6d35f6b774508",
+    "url": "/DrivingSchool/static/js/2.f7c536ad.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/DrivingSchool/static/js/2.f7c536ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "607ae823f9515955468e",
+    "url": "/DrivingSchool/static/js/main.5ef79a47.chunk.js"
+  },
+  {
+    "revision": "0f0b48cb95b790eef8ed",
+    "url": "/DrivingSchool/static/js/runtime-main.cbbd9f93.js"
+  },
+  {
+    "revision": "3158babfebe7e6e187ca4f9895c02879",
+    "url": "/DrivingSchool/static/media/car.3158babf.jpg"
+  }
+]);
